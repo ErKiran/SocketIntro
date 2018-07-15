3bff03867330303331b25152b4d7612dfeadd5e1
Kiran <enginerkiru@gmail.com>
2018-07-16 03:55:38 +0545

    Utils and test script Updated

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,5 @@
 var socket = io();
 
-function scrollToBottom () {
-  // Selectors
-  var messages = jQuery('#messages');
-  var newMessage = messages.children('li:last-child')
-  // Heights
-  var clientHeight = messages.prop('clientHeight');
-  var scrollTop = messages.prop('scrollTop');
-  var scrollHeight = messages.prop('scrollHeight');
-  var newMessageHeight = newMessage.innerHeight();
-  var lastMessageHeight = newMessage.prev().innerHeight();
-
-  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
-    messages.scrollTop(scrollHeight);
-  }
-}
-
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -34,7 +18,6 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -47,7 +30,6 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
